171c519ae141ab32361fbe81f196aeefac1de3f4
poker78 <mr.invoker@outlook.com>
2024-05-07 19:23:34 +0330

    add bot

--- entity/leagues.go
+++ entity/leagues.go
@@ -1,7 +1,9 @@
 package entity
 
 type LeagueDb struct {
-	ID    uint   `gorm:"primaryKey"`
-	Name  string `gorm:"unique"`
-	Bones uint64
+	ID            uint   `gorm:"primaryKey"`
+	Name          string `gorm:"unique"`
+	Bones         uint64
+	BalanceActive uint64
+	BalanceNext   uint64
 }
