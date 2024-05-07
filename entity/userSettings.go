06728b7e582fc596e94d28ed9c8930ece73c9148
poker78 <mr.invoker@outlook.com>
2024-05-07 22:57:47 +0330

    add type

--- entity/userSettings.go
+++ entity/userSettings.go
@@ -1,8 +1,8 @@
 package entity
 
 type UserSettingsDb struct {
-	ID          uint `gorm:"primaryKey"`
-	UserID      uint `gorm:"unique"`
+	ID          uint64 `gorm:"primaryKey"`
+	UserID      uint64 `gorm:"unique"`
 	Leagues     uint
 	LevelUser   uint
 	LevelTap    uint
