37b52c78c0d0523a0f378d1c94ed183d1d0ab149
poker78 <mr.invoker@outlook.com>
2024-05-08 14:35:26 +0330

    add type

--- entity/transform.go
+++ entity/transform.go
@@ -1,8 +1,8 @@
 package entity
 
 type TransformDb struct {
-	ID         uint `gorm:"primaryKey"`
-	UserID     uint `gorm:"unique"`
+	ID         uint   `gorm:"primaryKey"`
+	UserID     uint64 `gorm:"unique"`
 	TapAll     uint64
 	Balance    uint64
 	BalanceAll uint64
