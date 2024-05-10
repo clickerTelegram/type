ffeaf5d481141d9dc6e7a717ba757fc699df68c8
poker78 <mr.invoker@outlook.com>
2024-05-11 02:47:14 +0330

    lastupdate change

--- entity/lastUpdate.go
+++ entity/lastUpdate.go
@@ -3,14 +3,14 @@ package entity
 import "time"
 
 type LastUpdateOnlineDb struct {
-	ID         uint `gorm:"primaryKey"`
-	UserID     uint `gorm:"unique"`
+	ID         uint   `gorm:"primaryKey"`
+	UserID     uint64 `gorm:"unique"`
 	TimeOnline time.Time
 }
 
 type LastTapUserDb struct {
-	ID       uint `gorm:"primaryKey"`
-	UserID   uint `gorm:"unique"`
+	ID       uint   `gorm:"primaryKey"`
+	UserID   uint64 `gorm:"unique"`
 	CountTap uint64
 	TimeTap  time.Time
 }
