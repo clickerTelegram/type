1134b1c6a6700bf2ca586f90fd42348160bda023
poker78 <mr.invoker@outlook.com>
2024-05-17 04:16:16 +0330

    ساخت دیتابیس جدید

--- entity/tapLevel.go
+++ entity/tapLevel.go
@@ -1,7 +1,7 @@
 package entity
 
 type TapLevelsDB struct {
-	Id     int64 `json:"-"`
+	Id     int64 `json:"id"`
 	Rate   int64 `json:"rate"`
 	Energy int64 `json:"energy"`
 	Price  int64 `json:"price"`
