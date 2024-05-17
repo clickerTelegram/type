cd9a9965bbfa83cc0cbe37891328156406963de1
poker78 <mr.invoker@outlook.com>
2024-05-17 21:38:22 +0330

    ساخت گیم تایپ

---
+++ gameType/PlayerOnlineWeb.go
@@ -0,0 +1,19 @@
+package gameType
+
+type PlayerOnlineWeb struct {
+	PlayerId int64
+	Taps     int64
+	Shares   int64
+	Energy   int64
+	Earned   int64
+	Reward   int64
+	Spent    int64
+
+	LastTapPlayer int64
+
+	LevelTap     int64
+	LevelPlayer  int64
+	LevelEnergy  int64
+	LevelCharge  int64
+	LevelLeagues int64
+}
---
+++ gameType/gameStatus.go
@@ -0,0 +1,7 @@
+package gameType
+
+type GameStatus struct {
+	TapAll       int64
+	PlayerOnline int64
+	EarnedAll    int64
+}
