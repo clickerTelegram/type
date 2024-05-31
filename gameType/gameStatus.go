8d799ea8c6bb292d1ab1ab5b617a3781e16fb5be
poker78 <mr.invoker@outlook.com>
2024-05-31 10:42:59 +0330

    .

--- gameType/gameStatus.go
+++ gameType/gameStatus.go
@@ -3,8 +3,8 @@ package gameType
 import "math/big"
 
 type GameStatus struct {
-	TotalShareBalance big.Int
-	TotalTouches      big.Int
+	TotalShareBalance *big.Int
+	TotalTouches      *big.Int
 	TotalPlayers      int64
 	DailyUsers        int64
 	OnlinePlayers     int64
