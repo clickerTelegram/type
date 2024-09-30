0071059b3be69804564e33c344edf4dcc4dda6d3
poker78 <mr.invoker@outlook.com>
2024-09-30 08:50:05 +0330

    add type TokenClaim

---
+++ entity/tokenClaim.go
@@ -0,0 +1,14 @@
+package entity
+
+type TokenClaim struct {
+	Id       int64  `gorm:"primaryKey"`
+	PlayerId int64  `json:"player_id" gorm:"primaryKey;unique"`
+	Uuid     string `json:"uuid" gorm:"primaryKey;unique"`
+	Coin     int64
+	Leagues  int64
+	Level    int64
+	Ref      int64
+	Total    int64
+	IsFee    bool
+	CreateAt int64 `gorm:"autoCreateTime"`
+}
