4534e0d50ef031738cd9368152f6b776534bd06d
poker78 <mr.invoker@outlook.com>
2024-09-30 22:16:37 +0330

    add type REsGetToken

--- dto/res.player.getToken.go
+++ dto/res.player.getToken.go
@@ -1,12 +1,12 @@
 package dto
 
 type ResGetToken struct {
-	Total      int64  `json:"total"`
-	TokenCoins int64  `json:"token_coins"`
-	TokenRefs  int64  `json:"token_refs"`
-	TokenLig   int64  `json:"token_Ligue"`
-	TokenLevel int64  `json:"token_level"`
-	IsFee      bool   `json:"is_fee"`
-	Command    string `json:"command"`
-	TokenUSD   int64  `json:"token_usd"`
+	Total      int64   `json:"total"`
+	TokenCoins int64   `json:"token_coins"`
+	TokenRefs  int64   `json:"token_refs"`
+	TokenLig   int64   `json:"token_Ligue"`
+	TokenLevel int64   `json:"token_level"`
+	IsFee      bool    `json:"is_fee"`
+	Command    string  `json:"command"`
+	TokenUSD   float64 `json:"token_usd"`
 }
